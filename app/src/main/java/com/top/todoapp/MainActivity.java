03a80684c159ee9adf003b1aaabbb38c5969b542
Roman Yakushev <boxboxbox@inbox.ru>
2023-02-13 20:47:59 +0300

    Second commit

--- app/src/main/java/com/top/todoapp/MainActivity.java
+++ app/src/main/java/com/top/todoapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.top.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView tasksList = findViewById(R.id.listview1);
+        String[] tasks = getResources().getStringArray(R.array.tasks);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, tasks);
+        tasksList.setAdapter(adapter);
     }
 }
