73abf0009f8a4e7dbabe8416dd11ce1c122a77f4
Roman Yakushev <boxboxbox@inbox.ru>
2023-02-25 21:16:03 +0300

    All tasks done

--- app/src/main/java/com/top/todoapp/MainActivity.java
+++ app/src/main/java/com/top/todoapp/MainActivity.java
@@ -6,29 +6,61 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import android.widget.AdapterView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> tasks = new ArrayList<String>();
+    ArrayList<String> selectedTasks = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    ListView tasksList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView tasksList = findViewById(R.id.listview1);
-        String[] tasks = getResources().getStringArray(R.array.tasks);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, tasks);
+        Collections.addAll(tasks, "First", "Second", "Third");
+        tasksList = findViewById(R.id.listview1);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, tasks);
         tasksList.setAdapter(adapter);
 
-        Button btn = findViewById(R.id.button1);
-        btn.setOnClickListener(new View.OnClickListener() {
+        tasksList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                tasks.add("Clicked : "+clickCounter++);
-                adapter.notifyDataSetChanged();
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                String user = adapter.getItem(position);
+                if (tasksList.isItemChecked(position))
+                    selectedTasks.add(user);
+                else
+                    selectedTasks.remove(user);
             }
         });
+    }
 
+    public void add_task(View view) {
+        EditText taskName = findViewById(R.id.task_name);
+        String task = taskName.getText().toString();
+        if (!task.isEmpty()) {
+            adapter.add(task);
+            taskName.setText("");
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    public void remove_task(View view) {
+        for (int i = 0; i < selectedTasks.size(); i++) {
+            adapter.remove(selectedTasks.get(i));
+        }
+
+        tasksList.clearChoices();
+        selectedTasks.clear();
+
+        adapter.notifyDataSetChanged();
     }
 }
