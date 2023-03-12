68ee05ed7dd4a55ffc10fac713d89093118e76ce
Roman Yakushev <boxboxbox@inbox.ru>
2023-03-12 20:24:01 +0300

    Add Models Student and Tag

---
+++ app/src/main/java/com/top/todoapp/models/Student.java
@@ -0,0 +1,83 @@
+package com.top.todoapp.models;
+
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(int id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj instanceof Student) {
+            Student student = (Student) obj;
+            if (student.firstName.equals(this.firstName) &&
+                    student.lastName.equals(this.lastName) &&
+                    student.age == this.age && student.id == this.id) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
