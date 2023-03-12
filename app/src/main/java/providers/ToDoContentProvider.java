ac0cf063289bfa74d0a8facf49e0238ce2c6aafd
Roman Yakushev <boxboxbox@inbox.ru>
2023-03-12 19:41:01 +0300

    SQLLite ADD

--- app/src/main/java/providers/ToDoContentProvider.java
+++ app/src/main/java/providers/ToDoContentProvider.java
@@ -1,8 +1,14 @@
 package providers;
 
 import android.content.ContentProvider;
+import android.content.ContentValues;
 import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.HashMap;
 
@@ -20,5 +26,38 @@ public class ToDoContentProvider extends ContentProvider {
         uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
         uriMatcher.addURI(PROVIDER_NAME, "ToDos", TODOS);
         uriMatcher.addURI(PROVIDER_NAME, "TO_Dos/#", TODO_ID);
+    }
+
+    private SQLiteDatabase DB;
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public String getType( Uri uri) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public Uri insert( Uri uri, ContentValues values) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int delete( Uri uri, String selection, String[] selectionArgs) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int update( Uri uri, ContentValues contentValues, String s, String[] strings) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public Cursor query (Uri uri, String[] projection, String selection,
+                         String[] selectionArgs, String sortOrder){
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
